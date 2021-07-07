b3d62e9864f5eb8bb4d5f64b7929a08b949eecd8
Victor Okrobodo <vokrobodo@gmail.com>
2021-07-07 21:28:07 +0100

    updated bookmark controller tests

--- src/test/java/com/fixit/web/controller/BookMarkControllerTest.java
+++ src/test/java/com/fixit/web/controller/BookMarkControllerTest.java
@@ -1,13 +1,38 @@
 package com.fixit.web.controller;
 
+import com.fixit.web.service.BookMarkService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithUserDetails
+@ActiveProfiles("test")
 class BookMarkControllerTest {
 
+    @Autowired
+    private BookMarkService bookMarkService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    final int FIRST_PAGE = 1;
+
     @Test
-    void getStates() {
+    void getStates() throws Exception{
+        mockMvc.perform(get("/bookmarks/page/" + FIRST_PAGE))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bookmarks"))
+                .andExpect(model().attributeExists("currentPage"))
+                .andExpect(view().name("bookmarks/list"));
     }
 
     @Test
